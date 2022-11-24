488d829a11bace807f3809c1ffed601069800bfe
EquitysoftVinamra <vinamra@equitysoft.in>
2022-11-24 18:59:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 
 // console.log("hello equitysoft");
 
-// declare a variable
-// ->we can change value of variable
+// // declare a variable
+// // ->we can change value of variable
 
 // var firstName = "Vinamra";
 // console.log(firstName);
@@ -13,37 +13,79 @@
 // firstName = "Harshish";
 // console.log(firstName);
 
-// rules for naming variable
+// // rules for naming variable
 
-// note you can use $firsrname as variable name 
+// // note you can use $firsrname as variable name 
 
 // var value1 = 2;
 // console.log(value1 ** 3);
 
-// let VS Var
-// -> you can define value with same variable in let keyword
+// // let VS Var
+// // -> you can define value with same variable in let keyword
 // let Fname = "viru";
 // console.log(Fname);
 
 // let Fname = "pratik";
 // console.log(Fname);
 
-//Const in javascript
+// Const in javascript
 
-//-> We can not change the value of const
+// // -> We can not change the value of const
 
 // const pi = 3.14;
 // console.log(pi);
 
-// String Indexing
+// <------------------ String Indexing -------------------->
 
 // let myName = "Vinamra";
 // console.log(myName[5]);
 // console.log(myName.length); // find length of string
 
-let yourName = "   veer    "; 
-console.log(yourName.length)
+// let yourName = "veer"; 
+// console.log(yourName.length)
 
-yourName = yourName.trim();
-console.log(yourName)
-console.log(yourName.length)
+// yourName = yourName.trim();
+// console.log(yourName);
+// console.log(yourName.length);
+
+// yourName = yourName.toUpperCase();
+// yourName = yourName.toLowerCase();
+// console.log(yourName);
+
+// let demo = yourName.slice(0,3);
+// console.log(demo);
+
+// let age = 22;
+// let myname = "partth";
+// console.log(typeof myname);
+
+// Convert number to string
+
+// age = age + "";
+// console.log(typeof(age));
+
+// Convert  string to number
+//-> make + sign for convert a string to number
+
+// let myStr = +"34";
+// console.log(typeof myStr);
+
+// let age = 15;
+// console.log(typeof age);
+
+// String Concatination
+
+// let string1 = "viru";
+// let string2 = "patel";
+
+// let fullName = string1+ " " +string2
+// console.log(fullName);
+  
+// let num1 = "128";
+// let num2 = "120";
+
+// let newNum = +num1+ + +num2   // add + sign to convert string into the number 
+// console.log(newNum);
+// console.log(typeof newNum);
+
+// let aboutMe = "my name is "
