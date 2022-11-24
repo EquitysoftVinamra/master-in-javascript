1b6d521b862514c12fbfcfb59cfa5e3fb0f3f4e1
EquitysoftVinamra <vinamra@equitysoft.in>
2022-11-24 18:02:55 +0530

    --initial commit
    --initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// ->console.log can print anything in console
+
+// console.log("hello equitysoft");
+
+// declare a variable
+// ->we can change value of variable
+
+// var firstName = "Vinamra";
+// console.log(firstName);
+
+// firstName = "Harshish";
+// console.log(firstName);
+
+// rules for naming variable
+
+// note you can use $firsrname as variable name 
+
+// var value1 = 2;
+// console.log(value1 ** 3);
+
+// let VS Var
+// -> you can define value with same variable in let keyword
+// let Fname = "viru";
+// console.log(Fname);
+
+// let Fname = "pratik";
+// console.log(Fname);
+
+//Const in javascript
+
+//-> We can not change the value of const
+
+// const pi = 3.14;
+// console.log(pi);
+
+// String Indexing
+
+// let myName = "Vinamra";
+// console.log(myName[5]);
+// console.log(myName.length); // find length of string
+
+let yourName = "   veer    "; 
+console.log(yourName.length)
+
+yourName = yourName.trim();
+console.log(yourName)
+console.log(yourName.length)
